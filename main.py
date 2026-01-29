869353cd73d1a4f850357fe8a74eb11e36fe2d04
Vova <vovakucin082@gmail.com>
2026-01-29 13:45:16 +0100

    Added dictionary query via get

--- main.py
+++ main.py
@@ -11,13 +11,13 @@ def main() -> None:
     for name, info in data.items():
         guild = None
         nickname = name
-        email = info["email"]
-        bio = info["bio"]
-        race_data = info["race"]
-        name_race = race_data["name"]
-        description_race = race_data["description"]
-        skills_race = race_data["skills"]
-        guild_inf = info["guild"]
+        email = info.get("email")
+        bio = info.get("bio")
+        race_data = info.get("race")
+        name_race = race_data.get("name")
+        description_race = race_data.get("description")
+        skills_race = race_data.get("skills")
+        guild_inf = info.get("guild")
 
         race, created = Race.objects.get_or_create(
             name=name_race,
@@ -27,14 +27,14 @@ def main() -> None:
         if skills_race:
             for skill in skills_race:
                 Skill.objects.get_or_create(
-                    name=skill["name"],
-                    bonus=skill["bonus"],
+                    name=skill.get("name"),
+                    bonus=skill.get("bonus"),
                     race=race
                 )
 
         if guild_inf:
-            guild_name = guild_inf["name"]
-            guild_description = guild_inf["description"]
+            guild_name = guild_inf.get("name")
+            guild_description = guild_inf.get("description")
 
             guild, created = Guild.objects.get_or_create(
                 name=guild_name,
